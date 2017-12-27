3797490a4eb44099fe19b3f588d13a358bfcf950
Christopher Figueroa <christopher_figueroa@apple.com>
2017-12-26 20:06:24 -0800

    added cargo, added game.lua

---
+++ assets/scripts/game.lua
@@ -0,0 +1,25 @@
+-- the game script into the main.lua game loop. Add all other gameplay scripts to this
+
+
+local game = {}
+
+function game.load()
+
+
+
+end
+
+-- use to call update on all other objects
+function game.update(dt)
+
+
+end
+
+
+-- use to call draw on all other objects
+function game.draw()
+  love.graphics.setColor(20,255,0,255)
+  love.graphics.print("Hello", 100, 100)
+end
+
+return game
--- main.lua
+++ main.lua
@@ -5,10 +5,34 @@ tween = require("libs.tween")
 gif = require("libs.gif.GifCat")
 require("libs.lovebug")
 
+-- loads everthing in the assets folder
+assets = require('libs.cargo').init('assets')
+
+local game = require("assets.scripts.game")
+
+-- use to call load on all other lua objects
 function love.load(arg)
-  if arg[#arg] == "-debug" then require("mobdebug").start() end
+  game:load()
 end
+
+-- use to call update on all other objects
+function love.update(dt)
+  game:update(dt)
+end
+
+-- use to call draw on all other objects
 function love.draw()
-  love.graphics.setColor(20,255,0,255)
-  love.graphics.print("Hello", 100, 100)
+  game:draw()
+end
+
+function love.quit()
+  print("Thanks for playing! Come back soon!")
+end
+
+function love.focus(f)
+  if not f then
+    -- print("LOST FOCUS")
+  else
+    -- print("GAINED FOCUS")
+  end
 end
